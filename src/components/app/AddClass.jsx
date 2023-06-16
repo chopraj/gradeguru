a42f3386e0b63adf0f2bb0c2a9edb1914a82eb05
Raj Chopra <raj.chopra@vanderbilt.edu>
2023-06-15 23:08:47 -0700

    add slideover UI to add classes

---
+++ src/components/app/AddClass.jsx
@@ -0,0 +1,140 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { LinkIcon, PlusIcon, QuestionMarkCircleIcon } from '@heroicons/react/20/solid'
+
+
+export default function AddClass({open,setOpen}) {
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <div className="fixed inset-0" />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
+                  <form className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                    <div className="flex-1">
+                      {/* Header */}
+                      <div className="bg-gray-50 px-4 py-6 sm:px-6">
+                        <div className="flex items-start justify-between space-x-3">
+                          <div className="space-y-1">
+                            <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                              New class
+                            </Dialog.Title>
+                            <p className="text-sm text-gray-500">
+                              Get started by filling in the information below to create your new class.
+                            </p>
+                          </div>
+                          <div className="flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="text-gray-400 hover:text-gray-500"
+                              onClick={() => setOpen(false)}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* Divider container */}
+                      <div className="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
+                        {/* Project name */}
+                        <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                          <div>
+                            <label
+                              htmlFor="project-name"
+                              className="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5"
+                            >
+                              Class name
+                            </label>
+                          </div>
+                          <div className="sm:col-span-2">
+                            <input
+                              type="text"
+                              name="project-name"
+                              id="project-name"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        {/* Project description */}
+                        <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                          <div>
+                            <label
+                              htmlFor="project-description"
+                              className="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5"
+                            >
+                              Subject/ Period/ Description
+                            </label>
+                          </div>
+                          <div className="sm:col-span-2">
+                            <textarea
+                              id="project-description"
+                              name="project-description"
+                              rows={3}
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              defaultValue={''}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    {/* Maybe add an add students section here?*/}
+
+                    {/* Action buttons */}
+                    <div className="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
+                      <div className="flex justify-end space-x-3">
+                        <button
+                          type="button"
+                          className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                          onClick={() => setOpen(false)}
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          type="submit"
+                          className="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                          Create
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
--- src/components/app/Classes.jsx
+++ src/components/app/Classes.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { PlusIcon } from '@heroicons/react/20/solid'
 import Divider from '../utils/Divider'
+import AddClass from './AddClass'
 
-const NoClassesSection = () => {
+const NoClassesSection = ({open,setOpen}) => {
+
+  const handleAddClassBtn = () => {
+    setOpen(true);
+  }
+
   return (
   <div className="text-center">
       <svg
@@ -25,6 +31,7 @@ const NoClassesSection = () => {
       <div className="mt-6">
         <button
           type="button"
+          onClick={handleAddClassBtn}
           className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
@@ -36,6 +43,12 @@ const NoClassesSection = () => {
 }
 
 const Classes = () => {
+
+  const [slideoverOpen,setSlideoverOpen] = useState(true);
+
+  const handleAddClassBtn = () => {
+    setSlideoverOpen(true);
+  }
   return (
     <>
     <div className="md:flex md:items-center md:justify-between">
@@ -47,6 +60,7 @@ const Classes = () => {
       <div className="mt-4 flex md:ml-4 md:mt-0">
         <button
           type="button"
+          onClick={handleAddClassBtn}
           className="ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
@@ -55,7 +69,8 @@ const Classes = () => {
       </div>
     </div>
     <Divider/>
-    <NoClassesSection/>
+    <NoClassesSection open={slideoverOpen} setOpen={setSlideoverOpen}/>
+    <AddClass open={slideoverOpen} setOpen={setSlideoverOpen}/>
     </>
   )
 }
